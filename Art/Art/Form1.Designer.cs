f1ea3e9cb7e895e9d657906933de386ce65f7635
lkuipers <lolke.kuipers@gmail.com>
2020-02-11 13:42:08 +0100

    first commit

---
+++ Art/Art/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Art
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ivPbImage = new System.Windows.Forms.PictureBox();
+            this.ivBtnDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ivPbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ivPbImage
+            // 
+            this.ivPbImage.Location = new System.Drawing.Point(44, 74);
+            this.ivPbImage.Name = "ivPbImage";
+            this.ivPbImage.Size = new System.Drawing.Size(645, 447);
+            this.ivPbImage.TabIndex = 0;
+            this.ivPbImage.TabStop = false;
+            // 
+            // ivBtnDraw
+            // 
+            this.ivBtnDraw.Location = new System.Drawing.Point(308, 39);
+            this.ivBtnDraw.Name = "ivBtnDraw";
+            this.ivBtnDraw.Size = new System.Drawing.Size(75, 23);
+            this.ivBtnDraw.TabIndex = 1;
+            this.ivBtnDraw.Text = "Draw";
+            this.ivBtnDraw.UseVisualStyleBackColor = true;
+            this.ivBtnDraw.Click += new System.EventHandler(this.ivBtnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 592);
+            this.Controls.Add(this.ivBtnDraw);
+            this.Controls.Add(this.ivPbImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ivPbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ivPbImage;
+        private System.Windows.Forms.Button ivBtnDraw;
+    }
+}
+
---
+++ Art/Art/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Art
+{
+    public class Size
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+    }
+    public class Point
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Point(double aX, double aY)
+        {
+            x = aX;
+            y = aY;
+        }
+    }
+    public static class Square
+    {
+        public static List<Point> create(double x, double y, double size)
+        {
+            List<Point> path = new List<Point>() {
+                new Point( x - size, y - size ),
+                new Point( x + size, y - size ),
+                new Point( x + size, y + size ),
+                new Point( x - size, y + size )
+            };
+            return path;
+        }
+
+    }
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawPath(Graphics g, List<Point> p)
+        {
+            Pen pen = new Pen(Color.Red);
+            PointF[] pfa = new PointF[p.Count];
+
+            var el = p.ToArray();
+            for(int i = 0; i<p.Count; i++)
+            {
+                pfa[i].X = (float)el[i].x;
+                pfa[i].Y = (float)el[i].y;
+            }
+
+            g.DrawPolygon(pen, pfa);
+        }
+
+        public void CreateDrawing(Graphics g, Size dimensions)
+        {
+            float scale = 15;
+
+            var cx = dimensions.width / 2;
+            var cy = dimensions.height / 2;
+
+            List<List<Point>> lines = new List<List<Point>>();
+
+            for (int i = 0; i<12; i++)
+            {
+                double size = i + 1; // cm
+                size *= scale;
+                var margin = 0.25; // cm
+                margin *= scale;
+                lines.Add(Square.create(cx, cy, size));
+                lines.Add(Square.create(cx, cy, size + margin));
+            }
+
+            lines.ForEach(p => DrawPath(g,p));
+        }
+        private void ivBtnDraw_Click(object sender, EventArgs e)
+        {
+            int lImageWidth = ivPbImage.Size.Width;
+            int lImageHeight = ivPbImage.Size.Height;
+
+            Size dimensions = new Size { width = lImageWidth, height = lImageHeight };
+
+            var newImage = new Bitmap(lImageWidth, lImageHeight);
+
+            Graphics lGraphic = Graphics.FromImage(newImage);
+
+            lGraphic.FillRectangle(Brushes.AliceBlue, 0, 0, newImage.Width, newImage.Height);
+
+            CreateDrawing(lGraphic, dimensions);
+
+            ivPbImage.Image = newImage;
+        }
+    }
+}
