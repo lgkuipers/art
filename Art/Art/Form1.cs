1050236914ec4597eed1eab1da997cfcd6c8c7bf
lkuipers <lolke.kuipers@gmail.com>
2020-02-13 14:20:24 +0100

    clipping

--- Art/Art/Form1.cs
+++ Art/Art/Form1.cs
@@ -25,6 +25,15 @@ namespace Art
             x = aX;
             y = aY;
         }
+
+        public static bool operator==(Point a, Point b)
+        {
+            return ((a.x == b.x) && (a.y == b.y));
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            return ((a.x != b.x) || (a.y != b.y));
+        }
     }
     public static class Square
     {
@@ -50,11 +59,170 @@ namespace Art
             InitializeComponent();
         }
 
-        public List<List<Point>> clipPolylineToBox(List<Point> line, Point[] box)
+        const int INSIDE = 0; // 0000 
+        const int LEFT = 1; // 0001 
+        const int RIGHT = 2; // 0010 
+        const int BOTTOM = 4; // 0100 
+        const int TOP = 8; // 1000 
+
+
+        int computeCode(double x, double y, Point[] box)
+        {
+            // initialized as being inside 
+            int code = INSIDE;
+
+            if (x < box[0].x)   // to the left of rectangle 
+                code |= LEFT;
+            else if (x > box[1].x) // to the right of rectangle 
+                code |= RIGHT;
+            if (y < box[0].y)   // below the rectangle 
+                code |= BOTTOM;
+            else if (y > box[1].y) // above the rectangle 
+                code |= TOP;
+
+            return code;
+        }
+
+        // Implementing Cohen-Sutherland algorithm 
+        // Clipping a line from P1 = (x2, y2) to P2 = (x2, y2) 
+        bool cohenSutherlandClip(ref double x1, ref double y1,
+                                ref double x2, ref double y2,
+                                Point[] box)
+        {
+            // Compute region codes for P1, P2 
+            int code1 = computeCode(x1, y1, box);
+            int code2 = computeCode(x2, y2, box);
+
+            // Initialize line as outside the rectangular window 
+            bool accept = false;
+
+            while (true)
+            {
+                if ((code1 == 0) && (code2 == 0))
+                {
+                    // If both endpoints lie within rectangle 
+                    accept = true;
+                    break;
+                }
+                else if ((code1 & code2) != 0)
+                {
+                    // If both endpoints are outside rectangle, 
+                    // in same region 
+                    break;
+                }
+                else
+                {
+                    // Some segment of line lies within the 
+                    // rectangle 
+                    int code_out;
+                    double x = -999, y = -999;
+
+                    // At least one endpoint is outside the 
+                    // rectangle, pick it. 
+                    if (code1 != 0)
+                        code_out = code1;
+                    else
+                        code_out = code2;
+
+                    // Find intersection point; 
+                    // using formulas y = y1 + slope * (x - x1), 
+                    // x = x1 + (1 / slope) * (y - y1) 
+                    if ((code_out & TOP) != 0)
+                    {
+                        // point is above the clip rectangle 
+                        x = x1 + (x2 - x1) * (box[1].y - y1) / (y2 - y1);
+                        y = box[1].y;
+                    }
+                    else if ((code_out & BOTTOM) != 0)
+                    {
+                        // point is below the rectangle 
+                        x = x1 + (x2 - x1) * (box[0].y - y1) / (y2 - y1);
+                        y = box[0].y;
+                    }
+                    else if ((code_out & RIGHT) != 0)
+                    {
+                        // point is to the right of rectangle 
+                        y = y1 + (y2 - y1) * (box[1].x - x1) / (x2 - x1);
+                        x = box[1].x;
+                    }
+                    else if ((code_out & LEFT) != 0)
+                    {
+                        // point is to the left of rectangle 
+                        y = y1 + (y2 - y1) * (box[0].x - x1) / (x2 - x1);
+                        x = box[0].x;
+                    }
+
+                    // Now intersection point x,y is found 
+                    // We replace point outside rectangle 
+                    // by intersection point 
+                    if (code_out == code1)
+                    {
+                        x1 = x;
+                        y1 = y;
+                        code1 = computeCode(x1, y1, box);
+                    }
+                    else
+                    {
+                        x2 = x;
+                        y2 = y;
+                        code2 = computeCode(x2, y2, box);
+                    }
+                }
+            }
+
+            return accept;
+        }
+
+
+        public List<List<Point>> clipPolylineToBox(List<Point> polyline, Point[] box)
         {
             List<List<Point>> result = new List<List<Point>>();
 
-            result.Add(line);
+            var polyline_a = polyline.ToArray<Point>();
+            int n = polyline_a.Length;
+
+            var polyline_c = new List<Point>();
+
+            int i = 0;
+            var a = polyline_a[i];
+            Point b_new = polyline_a[i + 1];
+            bool new_b = false;
+            while (i < (n-1))
+            {
+                var b = polyline_a[i + 1];
+
+                double x1 = a.x;
+                double y1 = a.y;
+                double x2 = b.x;
+                double y2 = b.y;
+                bool r = cohenSutherlandClip(ref x1, ref y1, ref x2, ref y2, box);
+                var a_new = new Point(x1, y1);
+                b_new = new Point(x2, y2);
+                bool new_a = a != a_new;
+                new_b = b != b_new;
+                if (r)
+                {
+                    polyline_c.Add(a_new);
+                    if (new_b)
+                    {
+                        polyline_c.Add(b_new);
+                        List<Point> lp = new List<Point>(polyline_c);
+                        result.Add(lp);
+                        polyline_c.Clear();
+                    }
+                }
+                a = b;
+                i++;
+            }
+            if (!new_b)
+            {
+                polyline_c.Add(b_new);
+            }
+
+            if (polyline_c.Count != 0)
+            {
+                result.Add(polyline_c);
+            }
 
             return result;
         }
@@ -66,7 +234,10 @@ namespace Art
             foreach (var l in lines)
             {
                 List<List<Point>> r = clipPolylineToBox(l, box);
-                result.AddRange(r);
+                if (r.Count() > 0)
+                {
+                    result.AddRange(r);
+                }
             }
 
             return result;
@@ -95,7 +266,7 @@ namespace Art
 
             List<List<Point>> lines = new List<List<Point>>();
 
-            for (int i = 0; i<12; i++)
+            for (int i = 0; i<15; i++)
             {
                 double size = i + 1; // cm
                 size *= scale;
@@ -105,6 +276,8 @@ namespace Art
                 lines.Add(Square.createPolyline(cx, cy, size + margin));
             }
 
+            lines = clipPolylinesToBox(lines, box);
+
             lines.ForEach(p => DrawPath(g,p));
         }
         public void CreateDrawing2(Graphics g, Size dimensions, Point[] box, float scale)
@@ -112,10 +285,13 @@ namespace Art
             var cx = dimensions.width / 2;
             var cy = dimensions.height / 2;
 
-            int steps = 5;
+            int steps = 7;
             int count = 20;
-            double spacing = 1;
-            double radius = 2;
+            double spacing = 1; // cm
+            double radius = 2; // cm
+
+            //count = 1;
+            //radius = 21 * spacing;
 
             spacing *= scale;
             radius *= scale;
@@ -149,7 +325,7 @@ namespace Art
 
             Size dimensions = new Size { width = lImageWidth, height = lImageHeight };
 
-            double margin = 1.5;
+            double margin = 1.5; // cm
             margin *= scale;
 
             Point[] box = { new Point(margin, margin), new Point(dimensions.width - margin, dimensions.height - margin) };
@@ -162,7 +338,7 @@ namespace Art
             lGraphic.FillRectangle(Brushes.AliceBlue, 0, 0, (int) dimensions.width, (int) dimensions.height);
             lGraphic.DrawRectangle(Pens.Blue, (int) box[0].x, (int) box[0].y, (int) (box[1].x - box[0].x), (int) (box[1].y - box[0].y));
 
-            CreateDrawing2(lGraphic, dimensions, box, scale);
+            CreateDrawing1(lGraphic, dimensions, box, scale);
 
             ivPbImage.Image = newImage;
         }
